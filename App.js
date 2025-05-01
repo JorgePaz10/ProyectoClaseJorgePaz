719400daeeab014cf282c89f9587fa15252a9d65
DESKTOP-CG1VBVU\Joun <jpazarita10@gmail.com>
2025-05-01 16:42:45 -0600

    Mi Primer Proyecto 26 de Abril

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App(){
+
+    return(
+        <div>
+            <h1>Hola mundo desde React</h1>
+            <p>Creando mi Primera Aplicacion desde React</p>
+
+        </div>
+
+    );
+
+}
